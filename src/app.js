65141d99e70a11425bfc268bacb60b9b7e0bf1a3
israel <israelashenafi29@gmail.com>
2025-09-15 12:38:58 -0700

    OTP send

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import pool from './config/db.js'
 import chalk from 'chalk'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
 import user from './routes/auth.routes.js'
 
 dotenv.config()
@@ -10,6 +11,7 @@ const app = express()
 const port = process.env.PORT
 
 await pool.connect();
+app.use(cookieParser()); 
 
 app.use(express.json())
 app.use('/api/user', user)
